725117eb11b11d2c6e92ae1a4cce7e434c73901b
Niels <Nielsfeijen76@gmail.com>
2022-03-24 13:40:02 +0100

    viggo big dum

---
+++ RockstarsIT/RockstarsIT/Models/Tag.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RockstarsIT.Models
+{
+    public class Tag
+    {
+        private string _name;
+        private int TagId { get; set; }
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+    }
+}
--- RockstarsIT/RockstarsIT/Models/Tribe.cs
+++ RockstarsIT/RockstarsIT/Models/Tribe.cs
@@ -1,22 +1,62 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace RockstarsIT.Models
 {
     public class Tribe
     {
         private string _name;
-        private string _desctription;
+        private string _description;
+        private string _spotify;
+        private string _leadaddress;
+        private int _blocknumber;
+        private int _imagenumber;
+        private Tag _tag;
 
         public int TribeId { get; set; }
-        
+
+        private int? TagId { get; set; }
+        [ForeignKey(name: "TagId")]
+
+        public  virtual Tag tag
+        {
+            get => _tag;
+            set => _tag = value;
+        }
+
         public string Name
         {
             get => _name;
             set => _name = value;
         }
-        
-        public string Desctription
+
+        public string Description
         {
-            get => _desctription;
-            set => _desctription = value;
+            get => _description;
+            set => _description = value;
+        }
+
+        public string Spotify
+        {
+            get => _spotify;
+            set => _spotify = value;
+        }
+
+        public string LeadAddress
+        {
+            get => _leadaddress;
+            set => _leadaddress = value;
+        }
+
+        public int BlockNumber
+        {
+            get => _blocknumber;
+            set => _blocknumber = value;
+        }
+
+        public int ImageNumber
+        {
+            get => _imagenumber;
+            set => _imagenumber = value;
         }
     }
 }
