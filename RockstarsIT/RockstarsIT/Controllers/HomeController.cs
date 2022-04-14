618c90c8a33af445e73ed3e4ada6311fa61a9743
Abdelhadisaad <95355302+Abdelhadisaad@users.noreply.github.com>
2022-04-14 10:58:43 +0200

    hadi changes

--- RockstarsIT/RockstarsIT/Controllers/HomeController.cs
+++ RockstarsIT/RockstarsIT/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using RockstarsIT.Models;
 
@@ -12,15 +13,21 @@ namespace RockstarsIT.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly DatabaseContext _context;
+
+
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            string dataShowType = HttpContext.Request.Query["view"].ToString();
+            ViewData["DataShowType"] = dataShowType;
+            var databaseContext = _context.Article.Include(a => a.Rockstar);
+            return View(await databaseContext.ToListAsync());
         }
 
         public IActionResult buttons()
