0a8d887e801c986dc19f651f753bbf1962c2cebc
Cas Goorman <casgo98@hotmail.nl>
2022-04-20 13:10:41 +0200

    get content with highest viewcount

--- RockstarsIT/RockstarsIT/Controllers/HomeController.cs
+++ RockstarsIT/RockstarsIT/Controllers/HomeController.cs
@@ -12,14 +12,29 @@ namespace RockstarsIT.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly DatabaseContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DatabaseContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+
+            List<Article> articles = _context.Article.OrderByDescending(item => item.ViewCount).Take(5).ToList();
+            List<Video> videos = _context.Videos.OrderByDescending(item => item.ViewCount).Take(5).ToList();
+            List<Podcast> podcasts = _context.Podcasts.OrderByDescending(item => item.ViewCount).Take(5).ToList();
+
+            List<Object> cmonbined = new List<Object>();
+            cmonbined.Add(articles);
+            cmonbined.Add(videos);
+            cmonbined.Add(podcasts);
+
+            //articles.Select(a => a.ViewCount)
+            //List<int> test = _context.Article.Select(p => p.ViewCount);
+            //ViewBag.Students = GetStudents();
             return View();
         }
 
