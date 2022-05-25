2baef97f7384cd620820768a03fe92587f0d5a9e
LamersBart <92574075+LamersBart@users.noreply.github.com>
2022-05-25 13:17:34 +0200

    added podcast

---
+++ RockstarsIT/RockstarsIT/Models/Podcast.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RockstarsIT.Models
+{
+    public class Podcast
+    {
+        // Fields
+        private string _title;
+        private string _description;
+        private string _url;
+        private DateTime _dateCreated;
+        private DateTime _dateModified;
+        private DateTime _datePublished;
+        private bool _publishedStatus;
+        private int _viewCount;
+        // Properties
+        public int PodcastId { get; set; }
+        
+        public string Title
+        {
+            get => _title; 
+            set => _title = value;
+        }
+        public string Description
+        {
+            get => _description; 
+            set => _description = value;
+        }
+        public string URL
+        {
+            get => _url; 
+            set => _url = value;
+        }
+        public DateTime DateCreated
+        {
+            get => _dateCreated;
+            set => _dateCreated = value;
+        }
+        public DateTime DateModified
+        {
+            get => _dateModified;
+            set => _dateModified = value;
+        }
+        public DateTime DatePublished
+        {
+            get => _datePublished;
+            set => _datePublished = value;
+        }
+
+        public bool PublishedStatus
+        {
+            get => _publishedStatus;
+            set => _publishedStatus = value;
+        }
+        public int ViewCount
+        {
+            get => _viewCount; 
+            set => _viewCount = value;
+        }
+
+        public List<PodcastEpisode> PodcastEpisodes;
+
+        // Methods
+
+    }
+}
